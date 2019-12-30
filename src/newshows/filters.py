d2102192066817793c22e4039ca99149f11c60c4
Harald Fauland <hf@advokat.local>
2019-12-30 15:51:18 +0100

    added webchannel to showlist

--- src/newshows/filters.py
+++ src/newshows/filters.py
@@ -4,7 +4,8 @@ import django_filters
 
 class ShowFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr='icontains')
+    premiere = django_filters.DateFilter('premiere', label="premiered later than", lookup_expr='gt')
 
     class Meta:
         model = Show
-        fields = ['name', 'network', 'language', 'genre']
+        fields = ['name', 'network', 'webchannel', 'language', 'genre', 'premiere', 'imdb_id', 'status']
--- src/newshows/tables.py
+++ src/newshows/tables.py
@@ -1,10 +1,18 @@
 # tutorial/tables.py
 import django_tables2 as tables
 from .models import Show
+from django.utils.html import format_html
 
 
 class ShowTable(tables.Table):
     class Meta:
         model = Show
         # template_name = "django_tables2/bootstrap.html"
-        fields = ("name", "network", "genre", "language", "status", "premiere", "insonarr", "ignored" )
+        fields = ("name", "network", "webchannel", "genre", "language", "status", "premiere", "insonarr", "ignored", )
+
+    def render_name(self, value, record):
+        if record.imdb_id:
+            return format_html("{}<br/> <a href='https://www.imdb.com/title/{}'>IMDB</a>", value, record.imdb_id)
+        else:
+            return format_html("{}", value)
+   
