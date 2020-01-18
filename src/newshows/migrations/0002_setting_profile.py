d7c08c0cc06e6713360f76034c084a1758ece91e
Andrew Rogl <arogl1+github@gmail.com>
2020-01-18 13:45:12 +1000

    Clean up flake8 errors

--- src/newshows/migrations/0002_setting_profile.py
+++ src/newshows/migrations/0002_setting_profile.py
@@ -14,6 +14,9 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='setting',
             name='profile',
-            field=models.ForeignKey(blank=True, default=None, null=True, on_delete=django.db.models.deletion.CASCADE, to='newshows.Profile'),
+            field=models.ForeignKey(
+                blank=True, default=None, null=True, on_delete=django.db.models.deletion.CASCADE,
+                to='newshows.Profile'
+            ),
         ),
     ]
--- src/newshows/models.py
+++ src/newshows/models.py
@@ -128,7 +128,7 @@ class Setting(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True, null=True, default=None)
     addmonitored = models.BooleanField(default=True, verbose_name="Add shows as monitored to Sonnar")
     seasonfolders = models.BooleanField(default=True, verbose_name="Subfolders for seasons")
-    
+
     def __str__(self):
         return "Settings"
 
