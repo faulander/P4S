8f14c6cd9cbe968e0c48f381c619f69b9b9bd3b1
Harald Fauland <harald.fauland@gmail.com>
2020-01-24 17:06:50 +0100

    travis #6

--- src/newshows/tests.py
+++ src/newshows/tests.py
@@ -19,10 +19,6 @@ class StatusCodeTests(TestCase):
         response = self.client.get('/settings')
         self.assertEquals(response.status_code, 301)
 
-    def test_view_url_by_name(self):
-        response = self.client.get(reverse('lastSonarrDownloads'))
-        self.assertEquals(response.status_code, 200)
-
     def test_view_uses_correct_template(self):
         response = self.client.get(reverse('settings'))
         self.assertEquals(response.status_code, 200)
